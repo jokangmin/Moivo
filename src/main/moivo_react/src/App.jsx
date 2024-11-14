f262a287652f88c6b421b70ac34c71e74829350b
jokangmin <jomin5151@gmail.com>
2024-11-14 14:26:53 +0900

    디렉토리 추가 및 경로 수정

--- src/main/moivo_react/src/App.jsx
+++ src/main/moivo_react/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+<<<<<<< HEAD
 import Main_index from './components/Main_index';
+=======
+import Main_index from './components/main_index';
+import StoreBoard from './containers/Store/Store_board'; 
+import StoreList from './containers/Store/Store_list';
+>>>>>>> 3d7a206 (디렉토리 추가 및 경로 수정)
 import MainProvider from './contexts/MainContext';
 import ProductBoard from './components/product/product_board';
 import ProductList from './components/product/product_list';
+
 
 const App = () => {
   return (
