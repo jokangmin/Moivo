b00c75fa0fbd719650ff29fdb4daacc4167b0ccf
= <=>
2024-11-15 21:56:52 +0900

    "추가"

--- src/main/java/com/example/demo/payment/controller/PaymentController.java
+++ src/main/java/com/example/demo/payment/controller/PaymentController.java
@@ -2,7 +2,10 @@ package com.example.demo.payment.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.payment.service.PaymentService;
+
 @RestController
 public class PaymentController {
     
+    private PaymentService paymentService;
 }
