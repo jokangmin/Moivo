dcf8c8572e66280759ba423b37aa6ae431a64a8a
jaepro <https://github.com/jokangmin/Mini_project2.git>
2024-11-19 14:46:33 +0900

    jwt 하는 중

--- src/main/java/com/example/demo/user/dto/UserDTO.java
+++ src/main/java/com/example/demo/user/dto/UserDTO.java
@@ -3,5 +3,11 @@ package com.example.demo.user.dto;
 public class UserDTO { // 사용자 & 관리자
 
     private int userSeq;
-    
+    private String id;
+    private String name;
+    private String pwd;
+    private String email;
+    private String tel;
+    private String addr1, addr2;
+    private String gender;
 }
