28b1446e11342123f2ada8ff7319c96d49045b56
KimSeongChan97 <sck2270@gmail.com>
2024-11-22 11:56:51 +0900

    이그노어 삭제했어

--- src/main/moivo_react/src/containers/product/product_search.jsx
+++ src/main/moivo_react/src/containers/product/product_search.jsx
@@ -6,6 +6,7 @@ const prodict_search = () => {
      return (
           <div>
                <Banner />
+               <br/><br/><br/><br/><br/><br/>
                상품을 검색하세요
 
                전체 상품 중 검색하기...
@@ -13,6 +14,7 @@ const prodict_search = () => {
                검색 바 , 검색 아이콘 넣기
 
                검색 바 에 알맞은 텍스트 입력시 해당 값을 가지고 DB에서 검색하기
+               test !!
                <div>
                <Footer />
                </div>
